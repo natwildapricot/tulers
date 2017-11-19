f53b3e21dcce2015df0f5b71f2989e8cda7674c3
natwildapricot <nat@wildapricot.com>
2017-11-19 10:39:11 +0300

    Добавила счётчик (Домашка)

--- day3/in_class/03-game/js/script.js
+++ day3/in_class/03-game/js/script.js
@@ -5,14 +5,126 @@ window.addEventListener('load', function(){
 	const princess = document.querySelector('.princess')
 	const samurai = document.querySelector('.samurai')
 	const dragon = document.querySelector('.dragon')
+	let countPlayerId = document.querySelector('#countPlayerId')
+	let countComputerId = document.querySelector('#countComputerId')
+	CounterPlayer = 0
+	CounterComputer = 0
 
+
+													//прописываем путь
+	const bigImagePlayer = document.querySelector('.playerZone .bigImage')
+	const bigImageComputer = document.querySelector('.computerZone .bigImage')
+	const playBtn = document.querySelector('#go')
+	const result = document.querySelector('#resultId')
+
+
+	//собираем адреса картинок	//возвращает уникальный путь
 	const princessUrl = getComputedStyle(princess).backgroundImage
-	const samuraiUrl = getComputedStyle(princess).backgroundImage
-	const dragonUrl = getComputedStyle(princess).backgroundImage
+	const samuraiUrl = getComputedStyle(samurai).backgroundImage
+	const dragonUrl = getComputedStyle(dragon).backgroundImage
 
 	//изменяем большую картинку игрока
-	princess.addEventListener('click', function (){
+	princess.addEventListener('click', function () {
 		bigImagePlayer.style.backgroundImage = princessUrl
 	})
 
+	samurai.addEventListener('click', function () {
+		bigImagePlayer.style.backgroundImage = samuraiUrl
+	})	
+
+
+	dragon.addEventListener('click', function () {
+		bigImagePlayer.style.backgroundImage = dragonUrl
+	})
+ 	// логика компьютера
+	playBtn.addEventListener('click', function () {
+
+		let randomNumber = Math.floor(Math.random() * 3 ) + 1
+
+
+	switch(randomNumber) {
+		case 1:
+		bigImageComputer.style.backgroundImage = princessUrl
+		break
+
+		case 2:
+		bigImageComputer.style.backgroundImage = samuraiUrl
+		break
+
+		case 3:
+		bigImageComputer.style.backgroundImage = dragonUrl
+		break
+
+		default:
+		console.log('switch(randomNumber - error!)')
+	}
+
+	//сравнение результатов
+	const currentPlayerImage = getComputedStyle(bigImagePlayer).backgroundImage
+	const currentComputerImage = getComputedStyle(bigImageComputer).backgroundImage
 	
+	if (currentPlayerImage === currentComputerImage){
+		result.innerHTML = 'Ничья'
+
+		CounterPlayer = CounterPlayer + 1 
+		CounterComputer = CounterComputer + 1
+		
+		countPlayerId.innerHTML = CounterPlayer
+		countComputerId.innerHTML = CounterComputer
+
+			//princess
+	} else if (currentPlayerImage === princessUrl && currentComputerImage === samuraiUrl) {
+		rusult.innerHTML = 'Победил игрок. Принцесса соблазнила самурая'
+
+		CounterPlayer = CounterPlayer + 1 
+		countPlayerId.innerHTML = CounterPlayer
+		countComputerId.innerHTML = CounterComputer
+
+
+	} else if (currentPlayerImage === princessUrl && currentComputerImage === dragonUrl) {
+		rusult.innerHTML = 'Победил компьютер. Дракон похитил принцессу'
+
+		CounterComputer = CounterComputer + 1
+		countPlayerId.innerHTML = CounterPlayer
+		countComputerId.innerHTML = CounterComputer
+
+			//samurai
+	} else if (currentPlayerImage === samuraiUrl && currentComputerImage === princessUrl) {
+		result.innerHTML = 'Победил компьютер. Принцесса соблазнила самурая'
+
+		CounterComputer = CounterComputer + 1
+		countPlayerId.innerHTML = CounterPlayer
+		countComputerId.innerHTML = CounterComputer
+
+	} else if (currentPlayerImage === samuraiUrl && currentComputerImage === dragonUrl) {
+		result.innerHTML = 'Победил игрок. Самурай одолел дракона!'
+
+		CounterPlayer = CounterPlayer + 1 
+		countPlayerId.innerHTML = CounterPlayer
+		countComputerId.innerHTML = CounterComputer
+
+		//dragon
+	} else if (currentPlayerImage === dragonUrl && currentComputerImage === princessUrl) {
+		rusult.innerHTML = 'Победил игрок. Дракон похитил принцессу'
+
+		CounterPlayer = CounterPlayer + 1 
+		countPlayerId.innerHTML = CounterPlayer
+		countComputerId.innerHTML = CounterComputer
+		
+	} else if (currentPlayerImage === dragonUrl && currentComputerImage === samuraiUrl) {
+		rusult.innerHTML = 'Победил компьютер. Самурай одолел дракона'
+
+		CounterComputer = CounterComputer + 1
+		countPlayerId.innerHTML = CounterPlayer
+		countComputerId.innerHTML = CounterComputer
+
+	} else  {
+		result.innerHTML = 'Ты что, не умеешь играть? Выбери персонажа!'
+	}
+
+
+
+	})
+
+})
+
